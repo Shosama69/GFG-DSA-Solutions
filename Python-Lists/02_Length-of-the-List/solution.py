21fc841690daa41b62e02b29f271ad8a2bd648be
Shosama69 <rajatsingh20060@gmail.com>
2025-08-14 13:44:06 +0530

    Length-of-the-List

---
+++ Python-Lists/02_Length-of-the-List/solution.py
@@ -0,0 +1,18 @@
+"""
+Problem: Length-of-the-List
+Link: https://www.geeksforgeeks.org/problems/length-of-the-list/
+
+Description:
+You are given a list that contains integers. You need to return the length of the list.
+
+Example:
+Input: arr = [54, 43, 2, 1, 5]
+Output: 5
+
+Approach:
+len() function can be used to return the length of the list.
+For loop and a variable count can also be used to count the number of items in the list.
+"""
+
+def listLength(arr):
+    return len(arr)
