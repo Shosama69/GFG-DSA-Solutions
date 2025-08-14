b2b65d7dd33af3524ddc2555504ca6b39cf233e9
Shosama69 <rajatsingh20060@gmail.com>
2025-08-14 13:13:27 +0530

    Sum-of-the-List

---
+++ Python-Lists/Sum-of-the-List/solution.py
@@ -0,0 +1,20 @@
+"""
+Problem: Sum of the list.
+Link: https://www.geeksforgeeks.org/problems/sum-the-list/
+
+Description:
+You are given a list that contains integers. You need to return the sum of the list.
+
+Example:
+Input: arr = [54, 43, 2, 1, 5]
+Output: 105
+
+Approach:
+Sum of the list can be done using the sum() function.
+Sum of the list can also be done using for loop and a sum variable initiated to 0.
+"""
+
+def listSum(arr):
+    return sum(arr)
+    
+    
