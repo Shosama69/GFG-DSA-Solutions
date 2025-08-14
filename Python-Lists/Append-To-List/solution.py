45ccb3580e89c1032a6d9b2900c5db8e52039e32
Shosama69 <rajatsingh20060@gmail.com>
2025-08-14 13:36:06 +0530

    Append-To-List

---
+++ Python-Lists/Append-To-List/solution.py
@@ -0,0 +1,18 @@
+"""
+Problem: Append To List
+Link: https://www.geeksforgeeks.org/problems/append-to-list/
+
+Description:
+You are given three inputs a, b, c. You need to create a list and append a, b, c to the list and then return that list.
+
+Example:
+Input: a = 1, b = 2, c = 3
+Output: 1 2 3
+
+Approach:
+We can directly return the list with all three values in it.
+We can append all the items one by one and then return the list.
+"""
+
+def appendToList(a,b,c):
+    return [a,b,c]
