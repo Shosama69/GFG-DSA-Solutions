a5f5388e4b8d36ffcad1f59766c012ba4570495c
Rajat Singh <rajatsingh20060@gmail.com>
2025-08-18 13:32:27 +0530

    Remove-Duplicate-Elements-From-Sorted-Array

--- Python-Lists/08_Remove-Duplicate-Elements-From-Sorted-Array/solution.py
+++ Python-Lists/08_Remove-Duplicate-Elements-From-Sorted-Array/solution.py
@@ -11,17 +11,22 @@ Input: arr[] = [2, 2, 2, 2, 2]
 Output: [2]
 
 Approach:
-I've just used sets here because sets can only have unique elements.
-But there are more efficient methods of doing this.
+We can use the two pointer approach to keep track of the position of duplicate elements,
+and replacing them if any duplicates are there
 """
 
 
 class Solution:
     def removeDuplicates(self, arr):
-        no_duplicates = set(arr)
-        new_list = list(no_duplicates)
-        sorted_list = new_list.sort()
-        return new_list
+        if not arr:
+            return 0
+        k = 1
+        for i in range(1, len(arr)):
+            if arr[i] != arr[i - 1]:
+                arr[k] = arr[i]
+                k += 1
+
+        return arr[:k]
 
 
 
